c1a5ed073f4c2e063f8b488dde03bb6a653889f6
Ronak Warrier <ronak.w@qburst.com>
2025-01-09 00:46:04 +0530

    [BUGFIX] amazon logo missing and component folder structure update

--- amazon-clone-fe/src/app/layout.tsx
+++ amazon-clone-fe/src/app/layout.tsx
@@ -5,7 +5,7 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 
 import theme from "../theme";
-import AmazonHeader from "../../components/pageComponents/main/header/amazonHeader/AmazonHeader";
+import AmazonHeader from "../components/pageComponents/main/header/amazonHeader/AmazonHeader";
 
 const inter = Inter({ subsets: ["latin"] });
 
